27df345b7ff31d95b281aa3a5ca1f459251b1517
MerajShekh <mohdmeraj6@gmail.com>
2021-08-28 13:47:13 +0530

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello...')
+})
+
+app.get('/api/test', (req, res) => {
+    res.send([1, 2, 3])
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+})
