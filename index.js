5da5f9877300dd60f42d7d890c3fc0825ba4a7ef
MerajShekh <mohdmeraj6@gmail.com>
2021-08-28 16:43:50 +0530

    update index

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello...')
+    res.send('Welcome to Whatsapp API...')
 })
 
 app.get('/api/test', (req, res) => {
     res.send([1, 2, 3])
 })
 
+// Rida@6363
 
-const port = process.env.PORT || 3000;
+//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
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
